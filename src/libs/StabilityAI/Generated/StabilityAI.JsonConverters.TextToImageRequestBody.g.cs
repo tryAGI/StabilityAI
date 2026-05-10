3329617285f2c444ad31e8f2985cf998bd110ee1
Konstantin S. <havendv@gmail.com>
2026-05-10 06:31:29 +0400

    feat: Updated OpenAPI spec (#36)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/StabilityAI/Generated/StabilityAI.JsonConverters.TextToImageRequestBody.g.cs
+++ src/libs/StabilityAI/Generated/StabilityAI.JsonConverters.TextToImageRequestBody.g.cs
@@ -83,6 +83,7 @@ namespace StabilityAI.JsonConverters
             {
                 try
                 {
+
                     var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::StabilityAI.TextToImageRequestBodyVariant1), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::StabilityAI.TextToImageRequestBodyVariant1> ??
                                    throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::StabilityAI.TextToImageRequestBodyVariant1).Name}");
                     textToImageRequestBodyVariant1 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
@@ -93,9 +94,13 @@ namespace StabilityAI.JsonConverters
                 catch (global::System.InvalidOperationException)
                 {
                 }
+            }
 
+            if (textToImageRequestBodyVariant1 == null && generationOptionalParams == null)
+            {
                 try
                 {
+
                     var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::StabilityAI.GenerationRequestOptionalParams), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::StabilityAI.GenerationRequestOptionalParams> ??
                                    throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::StabilityAI.GenerationRequestOptionalParams).Name}");
                     generationOptionalParams = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
