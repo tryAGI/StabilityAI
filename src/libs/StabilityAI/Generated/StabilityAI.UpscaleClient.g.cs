e19101a0622ec01213c4ef15b0f63eb1dcec78d7
Konstantin S. <havendv@gmail.com>
2026-08-14 19:45:35 +0400

    feat: Updated OpenAPI spec (#50)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/StabilityAI/Generated/StabilityAI.UpscaleClient.g.cs
+++ src/libs/StabilityAI/Generated/StabilityAI.UpscaleClient.g.cs
@@ -40,10 +40,17 @@ namespace StabilityAI
 
         /// <inheritdoc/>
         public global::StabilityAI.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::StabilityAI.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::StabilityAI.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
